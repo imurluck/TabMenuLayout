8e7dfffc49d8cc505241813e930a1e532ade20c2
imurluck <1289042324@qq.com>
2020-03-23 21:33:10 +0800

    [feature]: water drop drawing

--- tabmenu/src/main/java/com/imurluck/tabmenu/TabMenuDrawable.kt
+++ tabmenu/src/main/java/com/imurluck/tabmenu/TabMenuDrawable.kt
@@ -8,6 +8,7 @@ import android.graphics.drawable.Drawable
 import android.util.Log
 import androidx.interpolator.view.animation.FastOutSlowInInterpolator
 import java.lang.ref.WeakReference
+import kotlin.math.abs
 import kotlin.math.sqrt
 
 /**
@@ -24,13 +25,27 @@ class TabMenuDrawable(
 
     private val radius = context.resources.getDimensionPixelOffset(R.dimen.select_circle_radius)
 
+    private val radiusUnit = radius / 10.0F
+
     private var currentX = 500.0F
 
     private var currentY = topDecorationHeight + radius * 6.0F / 10.0F
 
+    private val maxCircleChangeRatio = 0.4F
+
+    private var currentCircleChangeRatio = 0.0F
+
+    private var waterDropTopPoint = PointF(currentX, currentY - radius + radius * currentCircleChangeRatio)
+    private var waterDropBottomPoint = PointF(currentX, currentY + radius - radius * currentCircleChangeRatio)
+    private var waterDropLeftPoint = PointF(currentX - radius - radius * currentCircleChangeRatio, currentY)
+    private var waterDropRightPoint = PointF(currentX + radius + radius * currentCircleChangeRatio, currentY)
+
+    private val tempItemWidth = 300F
+
     private val topDecorationPaint = Paint().apply {
         style = Paint.Style.FILL
         color = Color.parseColor("#FBFCFE")
+        strokeWidth = 1.5F
         isAntiAlias = true
     }
 
@@ -40,7 +55,7 @@ class TabMenuDrawable(
         isAntiAlias = true
     }
 
-    private val circlePaint = Paint().apply {
+    private val waterDropPaint = Paint().apply {
         style = Paint.Style.FILL
         color = Color.parseColor("#0D9E74")
         isAntiAlias = true
@@ -50,18 +65,41 @@ class TabMenuDrawable(
 
     private val topDecorationPath = Path()
 
-    private val animator = ValueAnimator().apply {
-        setFloatValues(0.0F, radius * 4.0F / 10.0F)
+    private val waterDropPath = Path()
+
+//    private val animator = ValueAnimator().apply {
+//        setFloatValues(0.0F, radius * 6.0F / 10.0F)
+//        interpolator = FastOutSlowInInterpolator()
+//        duration = 1000L
+//        val originY = currentY
+//        addUpdateListener {
+//            currentY = originY + it.animatedValue as Float
+//            invalidateSelf()
+//        }
+//        repeatMode = REVERSE
+//        repeatCount = ValueAnimator.INFINITE
+//        startDelay = 2000L
+//        start()
+//    }
+
+    private val moveAnimator = ValueAnimator().apply {
+        setFloatValues(0.0F, tempItemWidth)
         interpolator = FastOutSlowInInterpolator()
-        duration = 1000L
-        val originY = currentY
+        duration = 2000L
+        val originX = currentX
         addUpdateListener {
-            currentY = originY + it.animatedValue as Float
+            currentX = originX + it.animatedValue as Float
+            val changeDestX = tempItemWidth * maxCircleChangeRatio
+            if (currentX <= originX + changeDestX) {
+                currentCircleChangeRatio = (currentX - originX) / changeDestX * maxCircleChangeRatio
+            } else if (currentX >= originX + tempItemWidth - changeDestX) {
+                currentCircleChangeRatio = (originX + tempItemWidth - currentX) / changeDestX * maxCircleChangeRatio
+            }
             invalidateSelf()
         }
         repeatMode = REVERSE
         repeatCount = ValueAnimator.INFINITE
-        startDelay = 2000L
+        startDelay = 500L
         start()
     }
 
@@ -71,8 +109,35 @@ class TabMenuDrawable(
         canvas.translate(bounds.left.toFloat(), bounds.top.toFloat())
         drawRectBackground(canvas)
         drawTopDecoration(canvas)
-        canvas.drawCircle(currentX, currentY, radius.toFloat(), circlePaint)
+        drawWaterDrop(canvas)
         canvas.restoreToCount(saveCount)
+    }
+
+    private fun drawWaterDrop(canvas: Canvas) {
+        waterDropTopPoint.x = currentX
+        waterDropTopPoint.y = currentY - radius + radius * currentCircleChangeRatio
+        waterDropBottomPoint.x = currentX
+        waterDropBottomPoint.y = currentY + radius - radius * currentCircleChangeRatio
+        waterDropLeftPoint.x = currentX - radius - radius * currentCircleChangeRatio
+        waterDropLeftPoint.y = currentY
+        waterDropRightPoint.x = currentX + radius + radius * currentCircleChangeRatio
+        waterDropRightPoint.y = currentY
+
+        waterDropPath.reset()
+        waterDropPath.moveTo(waterDropTopPoint.x, waterDropTopPoint.y)
+        waterDropPath.cubicTo(waterDropTopPoint.x + radius * CIRCLE_MAGIC_DIGIT, waterDropTopPoint.y,
+            waterDropRightPoint.x, waterDropRightPoint.y - radius * CIRCLE_MAGIC_DIGIT,
+            waterDropRightPoint.x, waterDropRightPoint.y)
+        waterDropPath.cubicTo(waterDropRightPoint.x, waterDropRightPoint.y + radius * CIRCLE_MAGIC_DIGIT,
+            waterDropBottomPoint.x + radius * CIRCLE_MAGIC_DIGIT, waterDropBottomPoint.y,
+            waterDropBottomPoint.x, waterDropBottomPoint.y)
+        waterDropPath.cubicTo(waterDropBottomPoint.x - radius * CIRCLE_MAGIC_DIGIT, waterDropBottomPoint.y,
+            waterDropLeftPoint.x, waterDropLeftPoint.y + radius * CIRCLE_MAGIC_DIGIT,
+            waterDropLeftPoint.x, waterDropLeftPoint.y)
+        waterDropPath.cubicTo(waterDropLeftPoint.x, waterDropLeftPoint.y - radius * CIRCLE_MAGIC_DIGIT,
+            waterDropTopPoint.x - radius * CIRCLE_MAGIC_DIGIT, waterDropTopPoint.y,
+            waterDropTopPoint.x, waterDropTopPoint.y)
+        canvas.drawPath(waterDropPath, waterDropPaint)
     }
 
     private fun drawRectBackground(canvas: Canvas) {
@@ -82,26 +147,26 @@ class TabMenuDrawable(
 
     private fun drawTopDecoration(canvas: Canvas) {
         decorationTopPoint.x = currentX
-        decorationTopPoint.y = currentY - radius - radius * 2.0F / 10.0F
+        decorationTopPoint.y = waterDropTopPoint.y - radius * 3.0F / 10.0F
         // decoration top height / 6
         val unit = ((topDecorationHeight - decorationTopPoint.y) / 6.0).toFloat()
         topDecorationPath.reset()
         topDecorationPath.moveTo(decorationTopPoint.x, decorationTopPoint.y)
-        topDecorationPath.cubicTo(currentX - 5 * unit, decorationTopPoint.y,
-            currentX - 8 * unit, decorationTopPoint.y + 2 * unit,
-            currentX - 10 * unit, decorationTopPoint.y + 3 * unit)
-        topDecorationPath.quadTo(currentX - 12 * unit, decorationTopPoint.y + 4 * unit,
-            currentX - 15 * unit, decorationTopPoint.y + 5 * unit)
-        topDecorationPath.quadTo(currentX - 18 * unit, decorationTopPoint.y + 6 * unit,
-            currentX - 20 * unit, decorationTopPoint.y + 6 * unit)
+        topDecorationPath.cubicTo(currentX - 5 * radiusUnit, decorationTopPoint.y,
+            currentX - 8 * radiusUnit, decorationTopPoint.y + 2 * unit,
+            currentX - 10 * radiusUnit, decorationTopPoint.y + 3 * unit)
+        topDecorationPath.quadTo(currentX - 12 * radiusUnit, decorationTopPoint.y + 4 * unit,
+            currentX - 15 * radiusUnit, decorationTopPoint.y + 5 * unit)
+        topDecorationPath.quadTo(currentX - 18 * radiusUnit, decorationTopPoint.y + 6 * unit + 0.5F,
+            currentX - 20 * radiusUnit, decorationTopPoint.y + 6 * unit + 0.5F)
 
-        topDecorationPath.lineTo(currentX + 20 * unit, decorationTopPoint.y + 6 * unit)
-        topDecorationPath.quadTo(currentX + 18 * unit, decorationTopPoint.y + 6 * unit,
-            currentX + 15 * unit, decorationTopPoint.y + 5 * unit)
-        topDecorationPath.quadTo(currentX + 12 * unit, decorationTopPoint.y + 4 * unit,
-            currentX + 10 * unit, decorationTopPoint.y + 3 * unit)
-        topDecorationPath.cubicTo(currentX + 8 * unit, decorationTopPoint.y + 2 * unit,
-            currentX + 5 * unit, decorationTopPoint.y,
+        topDecorationPath.lineTo(currentX + 20 * radiusUnit, decorationTopPoint.y + 6 * unit + 0.5F)
+        topDecorationPath.quadTo(currentX + 18 * radiusUnit, decorationTopPoint.y + 6 * unit + 0.5F,
+            currentX + 15 * radiusUnit, decorationTopPoint.y + 5 * unit)
+        topDecorationPath.quadTo(currentX + 12 * radiusUnit, decorationTopPoint.y + 4 * unit,
+            currentX + 10 * radiusUnit, decorationTopPoint.y + 3 * unit)
+        topDecorationPath.cubicTo(currentX + 8 * radiusUnit, decorationTopPoint.y + 2 * unit,
+            currentX + 5 * radiusUnit, decorationTopPoint.y,
             decorationTopPoint.x, decorationTopPoint.y)
         topDecorationPath.close()
         canvas.drawPath(topDecorationPath, topDecorationPaint)
@@ -118,5 +183,7 @@ class TabMenuDrawable(
 
     companion object {
         private const val TAG = "TabMenuDrawable"
+
+        private const val CIRCLE_MAGIC_DIGIT = 0.552284749831F
     }
 }
